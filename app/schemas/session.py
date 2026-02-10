732a1b954b1990469f0c803892a619ff8dd93aac
KlementevYP <ypklementev@icloud.com>
2026-02-10 18:29:01 +0300

    fix 2

--- app/schemas/session.py
+++ app/schemas/session.py
@@ -14,6 +14,9 @@ class SessionResponse(BaseModel):
     unique_id: str
     username: str
     connected_at: datetime
-    disconnected_at: datetime | None
+    disconnected_at: Optional[datetime]
     assigned_ip: str
     remote_ip: str
+
+    class Config:
+        orm_mode = True
