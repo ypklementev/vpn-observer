f38ec67e845c96b4bd45b1c3a63079dac15ae488
KlementevYP <ypklementev@icloud.com>
2026-02-10 18:50:16 +0300

    fix 5

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers.admin_router import router as admin_router
+from app.database import engine, Base
 from app.routers.lifecycle_router import router as lifecycle_router
 
 app = FastAPI(title="vpn-observer")
@@ -7,3 +8,8 @@ app = FastAPI(title="vpn-observer")
 app.include_router(lifecycle_router)
 
 app.include_router(admin_router)
+
+
+@app.on_event("startup")
+def create_tables():
+    Base.metadata.create_all(bind=engine)
