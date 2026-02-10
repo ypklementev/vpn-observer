e6c54e8bef315e9bca15b41a60811db1f22708fa
KlementevYP <ypklementev@icloud.com>
2026-02-10 18:45:11 +0300

    upd

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.routers import admin_router
 from app.routers.lifecycle_router import router as lifecycle_router
 
 app = FastAPI(title="vpn-observer")
 
 app.include_router(lifecycle_router)
+
+app.include_router(admin_router)
